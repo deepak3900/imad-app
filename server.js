45be15d6fe7bca96fad3b5c477f3f67606af7efd
deepak3900 <31006118+deepak3900@users.noreply.github.com>
2017-08-14 22:55:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('article1', function(req,res) {
-    res.send('requseted for article one');
+    res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 
  
